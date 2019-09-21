77e4532119fe9ced6e8e46d2cc820b22d8e58110
Jun <cjunho9868@gmail.com>
2019-09-21 10:11:49 +0700

    add menu

--- src/app/Driver.java
+++ src/app/Driver.java
@@ -17,5 +17,27 @@ public class Driver {
         System.out.println("||                                              ||");
         System.out.println("||                                              ||");
         System.out.println("--------------------------------------------------");
+        System.out.println();
+        System.out.println("To continue, please type in 'menu' to enter the main menu, or 'exit' to quit the program");
+        System.out.println();
+        System.out.print("enter a command: ");
+
+        String cmd = in.nextLine();
+        do {
+            if(!cmd.equals("menu")){
+                System.out.println("invalid command.");
+            }
+            System.out.print("enter a command: ");
+            cmd = in.nextLine();
+            System.out.println(cmd);
+        } while(!cmd.equals("menu"));
+
+        if(cmd.equals("menu")){
+            showMenu();
+        }
+    }
+
+    private static void showMenu(){
+        System.out.println("Hello");
     }
 }
